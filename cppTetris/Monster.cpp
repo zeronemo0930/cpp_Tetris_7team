40d3a4d3ce0c4abf76d261ffd14d3ccbcdc41de1
cmkcmk8771@naver.com <cmk8771@konkuk.ac.kr>
2025-05-31 00:23:54 +0900

    monster class

--- cppTetris/Monster.cpp
+++ cppTetris/Monster.cpp
@@ -1,27 +1,107 @@
 #include "Monster.h"
+#include "Monster.h"
+#include <iostream>
+
+using namespace std;
+
+int Monster::stage = 0;
 
 Monster::Monster(MonsterName name) : current_Monster(name){
 	
 	switch (name)
 	{
-	case MonsterName::MONSTER1:
+	case MonsterName::SLIME:
 		speed = 40;
 		stick_rate = 20;
 		hp = 10;
 		break;
-	case MonsterName::MONSTER2:
-		speed = 40;
-		stick_rate = 20;
-		hp = 10;
+	case MonsterName::GHOST:
+		speed = 20;
+		stick_rate = 15;
+		hp = 12;
 		break;
-	case MonsterName::MONSTER3:
-		speed = 40;
-		stick_rate = 20;
-		hp = 10;
+	case MonsterName::HAMSTER:
+		speed = 4;
+		stick_rate = 10;
+		hp = 1000;
 		break;
 	}
 }
 
 Monster::~Monster()
 {
+}
+
+void Monster::printMonster() const
+{	
+	switch (current_Monster) {
+	case MonsterName::SLIME:
+		cout << R"(
+     _____
+   /       \
+  |  o   o  |
+  |   ~~~   |
+   \_______/
+)" << endl;
+		break;
+
+	case MonsterName::GHOST:
+		cout << R"(
+     .-"      "-.
+   /    O  O     \
+  |      ■■■      |	
+  \   \_____/    /
+   "-.________.-"
+     /  |  |  \
+    /   |  |   \
+)" << endl;
+		break;
+
+	case MonsterName::HAMSTER:
+		cout << R"(
+    (\__/)
+  ( oㅅo )
+  / 　 づ♡
+ /|     |\
+(_|     |_)
+)" << endl;
+		break;
+	}
+
+}
+
+void Monster::printStatus() const {
+	cout << "========== Monster Info ==========\n";
+	switch (current_Monster) {
+	case MonsterName::SLIME:
+		cout << "Name       : Slime\n";
+		break;
+	case MonsterName::GHOST:
+		cout << "Name       : Ghost\n";
+		break;
+	case MonsterName::HAMSTER:
+		cout << "Name       : Hamster\n";
+		break;
+	}
+	cout << "HP         : " << hp << "\n";
+	cout << "==================================\n";
+}
+
+Monster* Monster::getNextMonster()		//monster 생성할 때 사용, 레벨 바뀔때 delete후 다시 사용
+{
+	if (stage == 0) {
+		stage++;
+		return new Monster(MonsterName::SLIME);
+	}
+	else if (stage == 1) {
+		stage++;
+		return new Monster(MonsterName::GHOST);
+	}
+	else if (stage == 2) {
+		stage++;
+		return new Monster(MonsterName::HAMSTER);
+	}
+	else {
+		return nullptr;
+	}
 }
--- cppTetris/Monster.h
+++ cppTetris/Monster.h
@@ -3,7 +3,7 @@
 
 // Monster 이름, 난이도 다시 정하기
 enum class MonsterName {
-	MONSTER1, MONSTER2, MONSTER3
+	SLIME, GHOST, HAMSTER
 };
 
 class Monster
@@ -12,10 +12,15 @@ public:
 	Monster() = delete;
 	Monster(MonsterName name);
 	~Monster();
+	void printMonster() const;
+	void printStatus() const;
+	void takeDamage() { hp--; }
+	bool isDead() const { return hp <= 0; }
+	static Monster* getNextMonster();
 
 private:
 	MonsterName current_Monster;
-
+	static int stage;
 	int speed;
 	int stick_rate;
 	int hp;
