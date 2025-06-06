3ee1ae698fcedb9802c1c42b2f30d95fe805a4fc
ryjun29 <happy042957@gmail.com>
2025-06-06 22:05:14 +0900

    그린조아

--- cppTetris/GameManager.cpp
+++ cppTetris/GameManager.cpp
@@ -145,14 +145,14 @@ void GameManager::init()
 	if (monster.stage == 0 && rand() % 3 == 0) randType = static_cast<Tetromino>(1);										// 네모 블록
 	
 	current_block.setBlock(randType);
-	if (monster.stage == 4) current_block.setGreenhateBlock(randType);
+	if (monster.stage == 3) current_block.setGreenhateBlock(randType);
 
 	current_block.setPos(5, 0);
 	randType = static_cast<Tetromino>(rand() % 7);
 	if(monster.stage == 0 && rand() % 3 == 0) randType = static_cast<Tetromino>(1);										// 네모 블록
 
 	next_block.setBlock(randType);
-	if (monster.stage == 4) current_block.setGreenhateBlock(randType);
+	if (monster.stage == 3) next_block.setGreenhateBlock(randType);
 	renderer.drawBlock(next_block, false);
 	shadowBlock(true);
 }
@@ -368,7 +368,7 @@ void GameManager::makeNewBlock()
 	randType = static_cast<Tetromino>(rand() % 8);
 	if(monster.stage == 0 && rand() % 3 == 0) randType = static_cast<Tetromino>(1);                            // 네모만 나오게 함
 	next_block.setBlock(randType);
-	if (monster.stage == 4) current_block.setGreenhateBlock(randType);
+	if (monster.stage == 3) next_block.setGreenhateBlock(randType);
 	renderer.drawBlock(next_block, false);
 	shadowBlock(true);
 	renderer.drawBlock(current_block, false);
