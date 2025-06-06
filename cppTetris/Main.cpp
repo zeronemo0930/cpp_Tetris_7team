48630f695be2b7025587daa4710779c46baf5f02
zeronemo0930 <zeronemo0930@gmail.com>
2025-06-06 17:48:14 +0900

    메모리 누수 체크 코드

--- cppTetris/Main.cpp
+++ cppTetris/Main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 #include "GameManager.h"
 using namespace std;
 
 int main() {
 	GameManager GM = GameManager();
 	GM.start();
-
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
